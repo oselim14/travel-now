d0a79986f81a93e5a6fc078c62838b0113c23cde
Mohamed Diop <yxngbizzy@gmail.com>
2021-11-05 13:24:17 -0700

    Basic crud UI

--- travelnow/settings.py
+++ travelnow/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,6 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'travelnow',
+        'PASSWORD': os.environ['POSTGRES']
     }
 }
 
