07436d0973fcd8b7887f904ec5fa6ac948632022
Tavi Selim <oselim14@gmail.com>
2021-11-09 10:15:33 -0800

    settings update

--- travelnow/settings_heroku.py
+++ travelnow/settings_heroku.py
@@ -117,11 +117,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 
 STATIC_URL = '/static/'
 
