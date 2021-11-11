60dbd0fb643213a9ed3a0584b9988bd6b3c5601f
Tavi Selim <oselim14@gmail.com>
2021-11-10 17:33:49 -0800

    drop down menu

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,8 +1,12 @@
 from django.forms import ModelForm
-from .models import Comment
+from .models import Comment, Itinerary
 
 class CommentForm(ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
 
+class LocationsForm(ModelForm):
+    class Meta:
+        model = Itinerary
+        fields = ['locations']
